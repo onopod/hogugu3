ed52a537c4e84f24f0ba3932433ba503ec8a268d
onopod <onopod@google.com>
2024-12-05 08:30:42 +0900

    メッセージ一覧

--- src/app/messagedetail/page.tsx
+++ src/app/messagedetail/page.tsx
@@ -1,17 +1,16 @@
 "use client"
 
-import MessageDetailList from "../components/MessageDetailList";
+import { Container } from "@mui/material";
 import AppBar from "../components/AppBar";
 import BottomBar from "../components/BottomBar";
+import MessageDetailList from "../components/MessageDetailList";
 export default function MessageDetail() {
     return (
         <>
             <AppBar />
-            <div className="items-center justify-items-center min-h-screen p-8">
-                <main className="flex flex-col gap-8">
-                    <MessageDetailList />
-                </main>
-            </div>
+            <Container maxWidth="sm">
+                <MessageDetailList />
+            </Container>
             <BottomBar />
         </>
     )
