0ae8e01043f5389944de0a73bc90123854cfd624
onopod <onopod@google.com>
2024-12-06 12:56:53 +0900

    まだ修正中

--- src/app/therapist/[id]/page.js
+++ src/app/therapist/[id]/page.js
@@ -1,53 +1,46 @@
 "use client"
-import { Box, Container, Rating, Typography } from '@mui/material';
+
+import BottomBar from "@/app/components/BottomBar";
+import { Container, Rating, Typography } from '@mui/material';
 import Image from "next/image";
 import { useParams } from 'next/navigation';
 import { useEffect, useState } from "react";
 import AppBar from '../../components/AppBar';
-import BookingForm from "../../components/BookingForm";
-import BottomBar from "../../components/BottomBar";
 
 export default function TherapistPage() {
     const params = useParams();
-    const [therapist, setTherapist] = useState(false);
+    const [therapist, setTherapist] = useState({})
+    const [menus, setMenus] = useState([]);
     useEffect(() => {
         fetch(`/api/therapists/${params.id}`)
-            .then((res) => res.json())
-            .then((data) => {
-                console.log(data)
-                setTherapist(data.therapist)
+            .then((data) => data.json())
+            .then((therapist) => {
+                setTherapist(therapist)
+                setMenus(therapist.menus)
             })
-    }, [])
+    })
     return (
         <>
             <AppBar />
             <Container maxWidth="sm">
-                <Box key={1}>
-                    <Image
-                        src="/paella.jpg"
-                        width={500}
-                        height={300}
-                        alt="" />
-                    <h1>{therapist.name}</h1>
-                    <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                        {therapist.comment}
-                    </Typography>
-
-                    <Rating name="half-rating"
-                        precision={0.5}
-                        size="small" value={4.5} />
-                    <div sx={{ ml: 2 }}>(4.5)</div>
-                    {
-                        therapist.menus?.map((menu, idx) => (
-                            <>
-                                <Typography key={idx}>{menu.menu.name}:{menu.treatmentTime}min {menu.price}円</Typography>
-                            </>
-
-                        ))
-                    }
-                </Box>
-
-                <BookingForm therapist={therapist} />
+                <Image
+                    src="/paella.jpg"
+                    width={500}
+                    height={300}
+                    alt="" />
+                <h1>{therapist.name}</h1>
+                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                    {therapist.comment}
+                </Typography>
+                <Rating name="half-rating"
+                    precision={0.5}
+                    size="small" value={4.5} />
+                <div sx={{ ml: 2 }}>(4.5)</div>
+                {menus.map((menu, idx) => (
+                    <>
+                        <Typography key={idx}>{menu.menu.name}:{menu.treatmentTime}min {menu.price}円</Typography>
+                    </>
+                ))}
             </Container >
             <BottomBar />
         </>
