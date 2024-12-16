a089d3a110647d5b632b990201446348cab6013e
onopod <onopod@google.com>
2024-12-16 22:15:10 +0900

    delete migrateions

--- prisma/migrations/20241215113553_status/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Status" (
-    "id" INTEGER NOT NULL,
-    "name" TEXT NOT NULL,
-
-    CONSTRAINT "Status_pkey" PRIMARY KEY ("id")
-);
--- prisma/migrations/20241216120220_/migration.sql
+++
@@ -1,2 +0,0 @@
--- DropIndex
-DROP INDEX "Reservation_userId_therapistMenuId_idx";
