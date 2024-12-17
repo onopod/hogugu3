9e2587d5579b6c6b8c163d0a21faa2f389c41b1f
onopod <onopod@gmail.com>
2024-12-17 13:23:52 +0900

    test

--- src/app/api/users/me/route.js
+++ src/app/api/users/me/route.js
@@ -61,6 +61,7 @@ export const PUT = async (req) => {
         });
         return NextResponse.json({ message: "Success", user }, { status: 200 });
     } catch (err) {
+        console.dir(err)
         return NextResponse.json({ message: "Error", err }, { status: 500 });
     }
 };
