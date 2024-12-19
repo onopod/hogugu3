0a24b01de14320e8947d7a23e143d5fe51ab779e
onopod <onopod@google.com>
2024-12-19 21:21:09 +0900

    review

--- src/app/components/Reviews.js
+++ src/app/components/Reviews.js
@@ -1,11 +1,22 @@
-import { Card } from "@mui/material";
+import { Avatar, Card, CardContent, CardHeader, Rating, Typography } from "@mui/material";
 
 export default function Reviews({ reviews }) {
     return (
         reviews && <div>
             {reviews?.map(review => (
-                <Card key={review.id}>
-                    {review.id}
+                <Card key={review.id}
+                    variant="outlined"
+                    sx={{ my: 2, width: "100%", overflow: 'auto' }}>
+                    <CardHeader avatar={<Avatar />}
+                        title={
+                            <Typography variant="h5">
+                                {`${review.user?.prefecture?.name || ""}${review.user?.city || ""}の${review.user?.gender?.name || ""}`}
+                            </Typography>}
+                    />
+                    <CardContent>
+                        <Typography>{review.comment}</Typography>
+                        <Rating value={review.rate} readOnly />
+                    </CardContent>
                 </Card>
             )
             )}
