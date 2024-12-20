022acdf2997a43347a6c6eb44fc10510888bcf60
onopod <onopod@gmail.com>
2024-12-20 18:00:17 +0900

    search

--- src/app/components/SearchDialog.js
+++ src/app/components/SearchDialog.js
@@ -1,88 +1,71 @@
-import { CloseIcon } from '@/app/icons';
-import { AppBar, Button, Dialog, DialogContent, IconButton, MenuItem, Select, Slide, Stack, TextField, Toolbar, Typography } from '@mui/material';
-import { Fragment, forwardRef, useState } from 'react';
-
-const Transition = forwardRef(function Transition(props, ref) {
-    return <Slide direction="up" ref={ref} {...props} />;
-});
+import { FormControl, Box, Button, Grid2 as Grid, MenuItem, Select, Stack, TextField, InputLabel, Grid2 } from '@mui/material';
 
 export default function FullScreenDialog({ prefectures, genders, menus, handleSubmit, onSubmit, register, setValue, watch, reset }) {
-    const [open, setOpen] = useState(false);
-
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
-    };
-
     const selectedPrefecture = watch("prefectureId");
     const selectedGender = watch("genderId");
     const selectedMenuId = watch("menuId");
 
     return (
-        <Fragment>
-            <Stack sx={{ mt: 1, mb: 1 }}>
-                <Button variant="outlined" onClick={handleClickOpen}>
-                    検索
-                </Button>
-            </Stack>
-            <Dialog
-                fullScreen
-                open={open}
-                onClose={handleClose}
-                TransitionComponent={Transition}
-            >
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <AppBar sx={{ position: 'relative' }}>
-                        <Toolbar>
-                            <IconButton
-                                edge="start"
-                                color="inherit"
-                                onClick={handleClose}
-                                aria-label="close"
-                            >
-                                <CloseIcon />
-                            </IconButton>
-                            <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
-                                検索
-                            </Typography>
-                        </Toolbar>
-                    </AppBar>
-                    <DialogContent>
-                        <Stack spacing={2}>
-                            地域
-                            <Select id="prefectureId"
+        <Box sx={{ my: 1, width: "100%" }}>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Grid2 container spacing={1}>
+                    <Grid2 size={6}>
+                        <FormControl variant="standard" fullWidth>
+                            <InputLabel id="prefectureId">地域</InputLabel>
+                            <Select labelId="prefectureId"
+                                id="prefectureId"
                                 value={selectedPrefecture}
                                 onChange={(e) => setValue("prefectureId", e.target.value)}
-                                displayEmpty
-                                label="prefecture">
+                                displayEmpty>
                                 {prefectures.map(prefecture => <MenuItem key={prefecture.id} value={prefecture.id}>{prefecture.name}</MenuItem>)}
                             </Select>
-                            性別
+                        </FormControl>
+                    </Grid2>
+                    <Grid2 size={6}>
+                        <FormControl variant="standard" fullWidth>
+                            <InputLabel id="genderId">性別</InputLabel>
                             <Select id="genderId"
+                                labelId="genderId"
                                 value={selectedGender}
                                 onChange={(e) => setValue("genderId", e.target.value)}
                                 displayEmpty
-                                label="gender">
+                            >
                                 {genders.map(gender => <MenuItem key={gender.id} value={gender.id}>{gender.name}</MenuItem>)}
                             </Select>
-                            メニュー
-                            <Select id="menuId"
+                        </FormControl>
+                    </Grid2>
+                    <Grid2 size={6}>
+                        <FormControl variant="standard" fullWidth>
+                            <InputLabel id="menuId">メニュー</InputLabel>
+                            <Select labelId="menuId"
+                                id="menuId"
                                 value={selectedMenuId}
                                 onChange={(e) => setValue("menuId", e.target.value)}
-                                displayEmpty
-                                label="menu">
+                                displayEmpty>
                                 {menus.map(menu => <MenuItem key={menu.id} value={menu.id}>{menu.name}</MenuItem>)}
                             </Select>
-                            <TextField id="freeWord" {...register("freeWord")} label="フリーワード" />
-                        </Stack>
-                        <Button type="submit" onClick={handleClose}>送信</Button>
-                        <Button onClick={() => reset()}>リセット</Button>
-                    </DialogContent>
-                </form>
-            </Dialog>
-        </Fragment >
+                        </FormControl>
+                    </Grid2>
+                    <Grid2 size={6}>
+                        <FormControl variant="standard" fullWidth>
+                            <TextField variant="standard" label="フリーワード"
+                                id="freeWord"
+                                {...register("freeWord")} />
+                        </FormControl>
+                    </Grid2>
+                    <Grid2 size={8}>
+                        <FormControl fullWidth>
+                            <Button variant="contained" type="submit">送信</Button>
+                        </FormControl>
+                    </Grid2>
+                    <Grid2 size={4}>
+                        <FormControl fullWidth>
+                            <Button variant="outlined" onClick={() => reset()}>リセット</Button>
+                        </FormControl>
+                    </Grid2>
+                </Grid2>
+
+            </form>
+        </Box >
     );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -9,13 +9,8 @@ import { useForm } from "react-hook-form";
 
 
 export default function Home() {
-  const pageSize = 10;
   const [itemCount, setItemCount] = useState(0);
   const [page, setPage] = useState(1);
-  const [prefectureId, setPrefectureId] = useState(null);
-  const [genderId, setGenderId] = useState(null);
-  const [menuId, setMenuId] = useState(null);
-  const [freeWord, setFreeWord] = useState(null)
   const [therapists, setTherapists] = useState([]);
   const [count, setCount] = useState(0);
   const [prefectures, setPrefectures] = useState([])
@@ -23,46 +18,7 @@ export default function Home() {
   const [menus, setMenus] = useState([])
   const [regions, setRegions] = useState([])
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setPrefectures(await getPrefectures())
-      setGenders(await getGenders())
-      setMenus(await getMenus())
-      setRegions(await getRegions())
-    }
-    fetchData();
-  }, [])
-
-
-  useEffect(() => {
-    const fetchTherapist = async () => {
-      const therapists = await getTherapists({ page, prefectureId, genderId, menuId, freeWord })
-      setTherapists(therapists);
-    }
-    fetchTherapist()
-
-    const fetchTherapistCount = async () => {
-      const itemCount = await getTherapistsCount({ page, prefectureId, genderId, menuId, freeWord })
-      setItemCount(itemCount);
-      setCount(Math.ceil(itemCount / pageSize))
-    }
-    fetchTherapistCount()
-  }, [page, prefectureId, genderId, menuId, freeWord])
-
-  const onPageChange = (_, page) => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-    setPage(page)
-  }
-
-  const onPrefectureClick = (prefectureId) => {
-    reset()
-    setPage(1)
-    setPrefectureId(prefectureId)
-    setMenuId(null)
-    setFreeWord("")
-  }
-
-  const { register, handleSubmit, setValue, reset, watch } = useForm({
+  const { register, handleSubmit, setValue, reset, watch, getValues } = useForm({
     defaultValues: {
       prefectureId: "",
       genderId: "",
@@ -70,12 +26,52 @@ export default function Home() {
       freeWord: "",
     },
   });
-  const onSubmit = data => {
-    setPrefectureId(data.prefectureId)
-    setGenderId(data.genderId)
-    setMenuId(data.menuId)
-    setFreeWord(data.freeWord)
+
+  const fetchMasterData = async () => {
+    setPrefectures(await getPrefectures())
+    setGenders(await getGenders())
+    setMenus(await getMenus())
+    setRegions(await getRegions())
   }
+
+  const fetchData = async () => {
+    const pageSize = 10;
+    const values = getValues();
+    const where = {
+      page,
+      ...(values.prefectureId ? { prefectureId: Number(values.prefectureId) } : {}),
+      ...(values.genderId ? { genderId: Number(values.genderId) } : {}),
+      ...(values.menuId ? { menuId: Number(values.menuId) } : {}),
+      ...(values.freeWord ? { freeWord: values.freeWord } : {})
+    }
+    const currentItemCount = await getTherapistsCount(where);
+    const currentTherapists = await getTherapists(where)
+    setTherapists(currentTherapists)
+    setItemCount(currentItemCount);
+    setCount(Math.ceil(currentItemCount / pageSize))
+  }
+
+  useEffect(() => {
+    fetchMasterData();
+    fetchData()
+    // eslint-disable-next-line
+  }, [])
+
+  const onPageChange = (_, page) => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+    setPage(page + 1)
+    fetchData()
+  }
+
+  const onPrefectureClick = (prefectureId) => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+    reset()
+    setValue("prefectureId", String(prefectureId));
+    fetchData()
+  }
+
+  const onSubmit = () => fetchData()
+
   const handleFavoriteClick = (therapistId) => {
     const toggleData = async (therapistId) => {
       await toggleFavorite(therapistId)
@@ -93,7 +89,8 @@ export default function Home() {
           menus={menus}
           handleSubmit={handleSubmit}
           onSubmit={onSubmit}
-          register={register} setValue={setValue}
+          register={register}
+          setValue={setValue}
           watch={watch}
           reset={reset} />
         {itemCount}件
