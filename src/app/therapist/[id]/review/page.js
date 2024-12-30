c6969973a66d3c8d484cd0e879685aaa388b96dd
onopod <onopod@google.com>
2024-12-31 08:12:28 +0900

    reviewページで取得するデータを修正

--- src/app/therapist/[id]/review/page.js
+++ src/app/therapist/[id]/review/page.js
@@ -1,17 +1,14 @@
-import { getReviews, getTherapist } from "@/app/actions";
 import { AppBar, BottomBar, Reviews, Therapist } from "@/app/components";
 import { Container } from "@mui/material";
 
 export default async function ReviewPage({ params }) {
     const id = Number((await (params)).id)
-    const therapist = await getTherapist(id);
-    const reviews = await getReviews(id);
     return (
         <>
             <AppBar />
             <Container maxWidth="sm">
-                <Therapist therapist={therapist} />
-                <Reviews reviews={reviews} />
+                <Therapist id={id} />
+                <Reviews id={id} />
             </Container>
             <BottomBar />
         </>
